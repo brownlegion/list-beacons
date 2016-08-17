08656b9788ceb08b364fed09218ba3e2c5cefbed
brownlegion <krishna.deoram@gmail.com>
2016-08-17 11:30:45 -0400

    fixed directory paths in beacon_scan

--- beacon_scan.sh
+++ beacon_scan.sh
@@ -8,36 +8,36 @@ echo "Scan "$COUNTER
 echo ""
 
 
-cat /home/pi/list-beacons/kdawg/found.txt > /home/pi/list-beacons/kdawg/previous.txt
-sudo node /home/pi/list-beacons/index.js > /home/pi/list-beacons/kdawg/allbeacons.txt &
+cat /usr/src/app/list-beacons/kdawg/found.txt > /usr/src/app/list-beacons/kdawg/previous.txt
+sudo node /usr/src/app/list-beacons/index.js > /usr/src/app/list-beacons/kdawg/allbeacons.txt &
 
 TASK_PID=$!
 sleep 10
 sudo kill  $TASK_PID
 
-cat /home/pi/list-beacons/kdawg/allbeacons.txt  | cut -d':' -f 2 | sort | uniq > /home/pi/list-beacons/kdawg/found.txt
-cat /home/pi/list-beacons/kdawg/found.txt
+cat /usr/src/app/list-beacons/kdawg/allbeacons.txt  | cut -d':' -f 2 | sort | uniq > /usr/src/app/list-beacons/kdawg/found.txt
+cat /usr/src/app/list-beacons/kdawg/found.txt
 
 echo " "
 echo "Compare Found : Previous"
 
-diff -y --suppress-common-lines /home/pi/list-beacons/kdawg/found.txt /home/pi/list-beacons/kdawg/previous.txt
+diff -y --suppress-common-lines /usr/src/app/list-beacons/kdawg/found.txt /usr/src/app/list-beacons/kdawg/previous.txt
 echo ""
 
 echo "Coming"
-diff  /home/pi/list-beacons/kdawg/previous.txt /home/pi/list-beacons/kdawg/found.txt | grep ">" | cut -d'>' -f 2 > /home/pi/list-beacons/kdawg/coming.txt
-cat /home/pi/list-beacons/kdawg/coming.txt
+diff  /usr/src/app/list-beacons/kdawg/previous.txt /usr/src/app/list-beacons/kdawg/found.txt | grep ">" | cut -d'>' -f 2 > /usr/src/app/list-beacons/kdawg/coming.txt
+cat /usr/src/app/list-beacons/kdawg/coming.txt
 echo ""
 
 echo "Going"
 ##diff --new-line-format='%L' kdawg/found.txt kdawg/previous.txt #> kdawg/going.txt
-diff --suppress-common-lines /home/pi/list-beacons/kdawg/found.txt /home/pi/list-beacons/kdawg/previous.txt | grep ">" | cut -d'>' -f 2 > /home/pi/list-beacons/kdawg/going.txt
+diff --suppress-common-lines /usr/src/app/list-beacons/kdawg/found.txt /usr/src/app/list-beacons/kdawg/previous.txt | grep ">" | cut -d'>' -f 2 > /usr/src/app/list-beacons/kdawg/going.txt
 cat kdawg/going.txt
 echo ""
 
 #Do the stuff with the coming/going texts. 
 echo "Influx Ins"
-awk -v major="major=" -v minor=",minor=" -v device=",device=\"${HOSTNAME}\"" '{print major""$3""minor""$5""device}' /home/pi/list-beacons/kdawg/coming.txt |while read beacondata; do 
+awk -v major="major=" -v minor=",minor=" -v device=",device=\"${HOSTNAME}\"" '{print major""$3""minor""$5""device}' /usr/src/app/list-beacons/kdawg/coming.txt |while read beacondata; do 
 #curl -X GET "http://159.203.15.175/filemaker/beaconStatus.php?"${beacondata}
 #echo "influx -execute \"insert beacon,state=In "${beacondata}"\" -database=beaconDatabase" > kdawg/intemp.txt
 #bash kdawg/intemp.txt
@@ -48,7 +48,7 @@ done
 
 echo ""
 echo "Influx Outs"
-awk -v major="major=" -v minor=",minor=" -v device=",device=\"${HOSTNAME}\"" '{print major""$3""minor""$5""device}' /home/pi/list-beacons/kdawg/going.txt |while read beacondata; do
+awk -v major="major=" -v minor=",minor=" -v device=",device=\"${HOSTNAME}\"" '{print major""$3""minor""$5""device}' /usr/src/app/list-beacons/kdawg/going.txt |while read beacondata; do
 #curl -X GET "http://159.203.15.175/filemaker/beaconStatus.php?"${beacondata}
 #echo "influx -execute \"insert beacon,state=Out "${beacondata}"\" -database=beaconDatabase" > kdawg/outtemp.txt
 #bash kdawg/outtemp.txt
