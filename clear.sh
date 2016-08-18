0f90fd149784ba35bb899468f3bf4c2c8ca1ccfd
brownlegion <krishna.deoram@gmail.com>
2016-08-18 12:34:09 -0400

    fixed path for clearing files

--- clear.sh
+++ clear.sh
@@ -1,8 +1,8 @@
-echo "" > kdawg/allbeacons.txt
-echo "" > kdawg/coming.txt
-echo "" > kdawg/found.txt
-echo "" > kdawg/going.txt
-echo "" > kdawg/previous.txt
-echo "" > kdawg/intemp.txt
-echo "" > kdawg/outtemp.txt
+echo "" > /usr/src/app/list-beacons/kdawg/allbeacons.txt
+echo "" > /usr/src/app/list-beacons/kdawg/coming.txt
+echo "" > /usr/src/app/list-beacons/kdawg/found.txt
+echo "" > /usr/src/app/list-beacons/kdawg/going.txt
+echo "" > /usr/src/app/list-beacons/kdawg/previous.txt
+echo "" > /usr/src/app/list-beacons/kdawg/intemp.txt
+echo "" > /usr/src/app/list-beacons/kdawg/outtemp.txt
 echo "Cleared files"
