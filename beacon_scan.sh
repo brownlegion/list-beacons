f971328dfc3d709e034440fc8411496c2c23deaf
brownlegion <krishna.deoram@gmail.com>
2016-08-26 12:01:05 -0400

    got rid of useless comments to avoid confusion, and added good comments

--- beacon_scan.sh
+++ beacon_scan.sh
@@ -40,10 +40,10 @@ echo ""
 #Do the stuff with the coming/going texts. 
 echo "Influx Ins"
 awk -v major="major=" -v minor=",minor=" -v device=",device=\"${HOSTNAME}\"" '{print major""$3""minor""$5""device}' /usr/src/app/list-beacons/kdawg/coming.txt |while read beacondata; do 
+#This is used for CURLing to a Filemaker database.
 #curl -X GET "http://159.203.15.175/filemaker/beaconStatus.php?"${beacondata}
-#echo "influx -execute \"insert beacon,state=In "${beacondata}"\" -database=beaconDatabase" > kdawg/intemp.txt
-#bash kdawg/intemp.txt
 #echo "http://159.203.15.175/filemaker/beaconStatusTest.php?${beacondata}"
+#This is used for CURLing to an Influx database.
 echo "beacon,state=In "${beacondata}"" > /usr/src/app/list-beacons/kdawg/intemp.txt
 curl -i -XPOST 'http://localhost:8086/write?db=beaconDatabase' --data-binary @/usr/src/app/list-beacons/kdawg/intemp.txt
 done
@@ -51,10 +51,10 @@ done
 echo ""
 echo "Influx Outs"
 awk -v major="major=" -v minor=",minor=" -v device=",device=\"${HOSTNAME}\"" '{print major""$3""minor""$5""device}' /usr/src/app/list-beacons/kdawg/going.txt |while read beacondata; do
+#This is used for CURLing to a Filemaker database.
 #curl -X GET "http://159.203.15.175/filemaker/beaconStatus.php?"${beacondata}
-#echo "influx -execute \"insert beacon,state=Out "${beacondata}"\" -database=beaconDatabase" > kdawg/outtemp.txt
-#bash kdawg/outtemp.txt
 #echo "http://159.203.15.175/filemaker/beaconStatusTest.php?${beacondata}"
+#This is used for CURLing to an Influx database.
 echo "beacon,state=Out "${beacondata}"" > /usr/src/app/list-beacons/kdawg/outtemp.txt
 curl -i -XPOST 'http://localhost:8086/write?db=beaconDatabase' --data-binary @/usr/src/app/list-beacons/kdawg/outtemp.txt
 done
